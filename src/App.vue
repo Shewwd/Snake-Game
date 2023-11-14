6d7a15d1346786f9ffc1f03e9e4e2732caa2db01
Jake Shawd <90359526+Shewwd@users.noreply.github.com>
2023-11-14 19:16:12 +0000

    Now its clean

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  console.log("Hello World");
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <h1>Hello World</h1>
+  </main>
+</template>
+
+<style scoped>
+  header {
+    line-height: 1.5;
+  }
+</style>
