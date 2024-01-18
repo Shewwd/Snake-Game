7fd5caab3787e07d256cae131dd1ee5b99821417
Jake Shawd <90359526+Shewwd@users.noreply.github.com>
2024-01-18 20:26:08 +0000

    game

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import Welcome from './components/Welcome.vue';
 import Game from './components/game/Game.vue'
 
 export const routerHistory = createWebHistory();
@@ -8,7 +7,6 @@ export const routerHistory = createWebHistory();
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Welcome },
-    { path: '/game', component: Game },
+    { path: '/', component: Game }
   ],
 });
